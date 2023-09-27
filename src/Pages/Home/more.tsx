bba703d9e1271442a1caa0a2849c85211b9ca53c
Elul Asfaha <scorpiondota22@gmail.com>
2023-09-28 00:40:30 +0300

    will refactor editors pick later more and editor use the same patch of code. just different data

--- src/Pages/Home/more.tsx
+++ src/Pages/Home/more.tsx
@@ -1,15 +1,57 @@
+import CardHero from "../../components/CardHero";
+import Feature from "../../components/Feature";
 import SideBar from "../../components/SideBar";
+import { editorsPicks } from "../../data";
+import { featuresDetail } from "../../data";
 
+type cardDetailsType = {
+    imgUrl: string;
+    title: string;
+    link: string;
+    category: string;
+    main?: boolean;
+};
+type featuresDetailsType = {
+    imgUrl: string;
+    title: string;
+    link: string;
+    category: string;
+    main?: boolean;
+};
 const More = () => {
+    const featuresDetails: featuresDetailsType[] = featuresDetail;
+
+    const cardData: cardDetailsType = editorsPicks;
+
     return (
         <div className='w-full py-8 bg-gray-200'>
             <div className='w-full max-w-[1300px] mx-auto flex flex-col gap-5'>
                 <p className='border-l-2 pl-2 border-blue-400 text-gray-800 font-bold text-2xl'>
                     More around the BBC
                 </p>
-                <div className='grid grid-cols-3'>
+                <div className='grid md:grid-cols-3 gap-5'>
                     <SideBar
                         data={[
+                            {
+                                news: "asdasdsadsadsadasdasd",
+                                author: "asdasd",
+                                profile: "asdsad",
+                            },
+                            {
+                                news: "asdasdsadsadsadasdasd",
+                                author: "asdasd",
+                                profile: "asdsad",
+                            },
+                            {
+                                news: "asdasdsadsadsadasdasd",
+                                author: "asdasd",
+                                profile: "asdsad",
+                            },
+                            {
+                                news: "asdasdsadsadsadasdasd",
+                                author: "asdasd",
+                                profile: "asdsad",
+                            },
                             {
                                 news: "asdasdsadsadsadasdasd",
                                 author: "asdasd",
@@ -35,7 +77,33 @@ const More = () => {
                         title='correspondents'
                         authors={true}
                     />
-                    <div className='col-span-2'></div>
+                    <div className='w-full col-span-2 flex flex-col gap-5'>
+                        <div>
+                            <div className='hidden md:flex'>
+                                <CardHero
+                                    imgUrl={cardData.imgUrl}
+                                    title={cardData.title}
+                                    category={cardData.category}
+                                    link={cardData.link}
+                                    main={true}
+                                    description='The aim was to hear rouge nuclear weapons tests. They discovered something else'
+                                />
+                            </div>
+                            <div className='lg:hidden'></div>
+                        </div>
+                        <div className='grid grid-cols-2 lg:grid-cols-3 gap-5'>
+                            {featuresDetails.map((items, index) => (
+                                <Feature
+                                    key={index}
+                                    imgDisp={items.imgUrl}
+                                    title={items.title}
+                                    description={items.title}
+                                    category={items.category}
+                                    background={true}
+                                />
+                            ))}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
